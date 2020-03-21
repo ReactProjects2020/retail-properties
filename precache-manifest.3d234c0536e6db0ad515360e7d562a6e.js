334ac0454978737bbf7fee4396e429d0753e3d9c
kshamta sinha <kshamta.sinha@gmail.com>
2020-03-21 23:26:49 +1000

    Updates

---
+++ precache-manifest.3d234c0536e6db0ad515360e7d562a6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f973c8505c7ee7895573abbef97f1410",
+    "url": "/ReactProjects2020/retail-properties/index.html"
+  },
+  {
+    "revision": "4bc0d420850adc749752",
+    "url": "/ReactProjects2020/retail-properties/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "cb8b5f3c7850c9aff3c1",
+    "url": "/ReactProjects2020/retail-properties/static/css/main.bfeab08c.chunk.css"
+  },
+  {
+    "revision": "4bc0d420850adc749752",
+    "url": "/ReactProjects2020/retail-properties/static/js/2.61a7f996.chunk.js"
+  },
+  {
+    "revision": "f35b81241ce0c42e0c0a918874f3aafb",
+    "url": "/ReactProjects2020/retail-properties/static/js/2.61a7f996.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb8b5f3c7850c9aff3c1",
+    "url": "/ReactProjects2020/retail-properties/static/js/main.ddce868d.chunk.js"
+  },
+  {
+    "revision": "e768050489036301444d",
+    "url": "/ReactProjects2020/retail-properties/static/js/runtime-main.257bfa90.js"
+  }
+]);
